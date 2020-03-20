446dc37ae9f6f10c848c03f96a9853dd32749e6f
Daniel Funseth <daniel.funseth@knowit.se>
2020-03-20 15:00:18 +0100

    1.0.0-beta.1

--- src/assets/TopImage.tsx
+++ src/assets/TopImage.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 
 const topimage = require('./Sthlm_28.jpg');
 
-export const TopImage: React.SFC = () => <img src={topimage} alt=" " />;
+export const TopImage: React.SFC = () => <img src={topimage} alt="Stockholm och blå himmel" />;
