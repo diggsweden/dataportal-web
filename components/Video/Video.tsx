6d792056f227d648860efe259975fcb4b302bb66
Alexander <alexander.volkers@knowit.se>
2023-01-27 14:19:39 +0100

    fix: dispose video player

--- components/Video/Video.tsx
+++ components/Video/Video.tsx
@@ -9,6 +9,7 @@ export const Video: React.FC<VideoProps> = ({ screen9 }) => {
   const status = useScript("https://cdn.screen9.com/players/amber-player.js");
   const containerid = `video_screen9_${screen9.id}`;
 
+  let player: any;
   useEffect(() => {
     if (status === "ready" && screen9?.id) {
       const options = {
@@ -17,7 +18,12 @@ export const Video: React.FC<VideoProps> = ({ screen9 }) => {
         token: env("SCREEN9_API_TOKEN"),
       };
 
-      new (window as any).screen9.Player(options);
+      player = new (window as any).screen9.Player(options);
+    }
+    return () => {
+      if (player) {
+        player.dispose();
+      }
     }
   }, [status]);
 
--- components/blocks/Media.tsx
+++ components/blocks/Media.tsx
@@ -11,7 +11,6 @@ import env from "@beam-australia/react-env";
 import { MediaBase } from "../../graphql/__generated__/MediaBase";
 import { responsive } from "../../styles/image";
 import { isExternalLink } from "../../utilities/checkers";
-import { useCallback } from "react";
 
 export const handleUrl = ({ screen9, url }: MediaBase) => {
   const isClientUrl = (url as any)?.src?.startsWith("/_next/static/media");
@@ -22,50 +21,50 @@ export const handleUrl = ({ screen9, url }: MediaBase) => {
   return (env("MEDIA_BASE_URL") || "") + url;
 };
 
-export const Media: React.FC<IMedia> = ({ heading, description, media }) => {
-  const renderMedia = useCallback((media: Media_media, mediaDescription?: string) => {
-    const { alt, description } = media;
-    const url = handleUrl(media);
-    switch (media.__typename) {
-      case "dataportal_Digg_Image":
-        if (!url || url === "") {
-          return null;
-        }
-        const width = media?.width || "";
-        const height = media?.height || "";
-        return (
-          <figure>
-            <Image
-              style={responsive}
-              src={url}
-              width={width || 600}
-              height={height || 400}
-              alt={alt || ""}
-            />
-            {mediaDescription && (
-              <figcaption>
-                <p className="media-description">{checkLang(mediaDescription)}</p>
-              </figcaption>
-            )}
-          </figure>
-        );
-      case "dataportal_Digg_Video":
-        return (
-          <figure role="group">
-            <Video {...media} />
-            {mediaDescription && (
-              <figcaption>
-                <p className="media-description">{checkLang(mediaDescription)}</p>
-              </figcaption>
-            )}
-          </figure>
-        );
-  
-      case "dataportal_Digg_File":
-        return <FileLink link={url}>{description || url}</FileLink>;
-    }
-  }, [media]);
+const renderMedia = (media: Media_media, mediaDescription?: string) => {
+  const { alt, description } = media;
+  const url = handleUrl(media);
+  switch (media.__typename) {
+    case "dataportal_Digg_Image":
+      if (!url || url === "") {
+        return null;
+      }
+      const width = media?.width || "";
+      const height = media?.height || "";
+      return (
+        <figure>
+          <Image
+            style={responsive}
+            src={url}
+            width={width || 600}
+            height={height || 400}
+            alt={alt || ""}
+          />
+          {mediaDescription && (
+            <figcaption>
+              <p className="media-description">{checkLang(mediaDescription)}</p>
+            </figcaption>
+          )}
+        </figure>
+      );
+    case "dataportal_Digg_Video":
+      return (
+        <figure role="group">
+          <Video {...media} />
+          {mediaDescription && (
+            <figcaption>
+              <p className="media-description">{checkLang(mediaDescription)}</p>
+            </figcaption>
+          )}
+        </figure>
+      );
 
+    case "dataportal_Digg_File":
+      return <FileLink link={url}>{description || url}</FileLink>;
+  }
+};
+
+export const Media: React.FC<IMedia> = ({ heading, description, media }) => {
   return (
     <div className="media">
       {heading && (
@@ -77,3 +76,4 @@ export const Media: React.FC<IMedia> = ({ heading, description, media }) => {
     </div>
   );
 };
+
