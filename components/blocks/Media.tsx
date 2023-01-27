547954856bab214d255534d45e6dbac30a741d6f
Alexander <alexander.volkers@knowit.se>
2023-01-27 11:27:38 +0100

    fix: videoplayer dissappears after refresh

--- components/blocks/Media.tsx
+++ components/blocks/Media.tsx
@@ -11,6 +11,7 @@ import env from "@beam-australia/react-env";
 import { MediaBase } from "../../graphql/__generated__/MediaBase";
 import { responsive } from "../../styles/image";
 import { isExternalLink } from "../../utilities/checkers";
+import { useCallback } from "react";
 
 export const handleUrl = ({ screen9, url }: MediaBase) => {
   const isClientUrl = (url as any)?.src?.startsWith("/_next/static/media");
@@ -21,50 +22,50 @@ export const handleUrl = ({ screen9, url }: MediaBase) => {
   return (env("MEDIA_BASE_URL") || "") + url;
 };
 
-const renderMedia = (media: Media_media, mediaDescription?: string) => {
-  const { alt, description } = media;
-  const url = handleUrl(media);
-  switch (media.__typename) {
-    case "dataportal_Digg_Image":
-      if (!url || url === "") {
-        return null;
-      }
-      const width = media?.width || "";
-      const height = media?.height || "";
-      return (
-        <figure>
-          <Image
-            style={responsive}
-            src={url}
-            width={width || 600}
-            height={height || 400}
-            alt={alt || ""}
-          />
-          {mediaDescription && (
-            <figcaption>
-              <p className="media-description">{checkLang(mediaDescription)}</p>
-            </figcaption>
-          )}
-        </figure>
-      );
-    case "dataportal_Digg_Video":
-      return (
-        <figure role="group">
-          <Video {...media} />
-          {mediaDescription && (
-            <figcaption>
-              <p className="media-description">{checkLang(mediaDescription)}</p>
-            </figcaption>
-          )}
-        </figure>
-      );
-
-    case "dataportal_Digg_File":
-      return <FileLink link={url}>{description || url}</FileLink>;
-  }
-};
-
 export const Media: React.FC<IMedia> = ({ heading, description, media }) => {
+  const renderMedia = useCallback((media: Media_media, mediaDescription?: string) => {
+    const { alt, description } = media;
+    const url = handleUrl(media);
+    switch (media.__typename) {
+      case "dataportal_Digg_Image":
+        if (!url || url === "") {
+          return null;
+        }
+        const width = media?.width || "";
+        const height = media?.height || "";
+        return (
+          <figure>
+            <Image
+              style={responsive}
+              src={url}
+              width={width || 600}
+              height={height || 400}
+              alt={alt || ""}
+            />
+            {mediaDescription && (
+              <figcaption>
+                <p className="media-description">{checkLang(mediaDescription)}</p>
+              </figcaption>
+            )}
+          </figure>
+        );
+      case "dataportal_Digg_Video":
+        return (
+          <figure role="group">
+            <Video {...media} />
+            {mediaDescription && (
+              <figcaption>
+                <p className="media-description">{checkLang(mediaDescription)}</p>
+              </figcaption>
+            )}
+          </figure>
+        );
+  
+      case "dataportal_Digg_File":
+        return <FileLink link={url}>{description || url}</FileLink>;
+    }
+  }, [media]);
+
   return (
     <div className="media">
       {heading && (
