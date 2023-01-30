282c456fd293523f8d56de6978d9f2e7d981975b
Alexander <alexander.volkers@knowit.se>
2023-01-30 14:45:01 +0100

    reverting removing amber, issues when deployed

--- components/Video/Video.tsx
+++ components/Video/Video.tsx
@@ -9,7 +9,6 @@ export const Video: React.FC<VideoProps> = ({ screen9 }) => {
   const status = useScript("https://cdn.screen9.com/players/amber-player.js");
   const containerid = `video_screen9_${screen9.id}`;
 
-  let player: any;
   useEffect(() => {
     if (status === "ready" && screen9?.id) {
       const options = {
@@ -18,18 +17,7 @@ export const Video: React.FC<VideoProps> = ({ screen9 }) => {
         token: env("SCREEN9_API_TOKEN"),
       };
 
-      player = new (window as any).screen9.Player(options);
-    }
-    return () => {
-      if (player) {
-        player.dispose();
-        const script = document.querySelector(
-          'script[src="https://cdn.screen9.com/players/amber-player.js"]'
-        );
-        if (script) {
-          script.remove();
-        }
-      }
+      new (window as any).screen9.Player(options);
     }
   }, [status]);
 
