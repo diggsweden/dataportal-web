d6c5006929733c55efd58a7d3c0f5eeb5530bf87
linulas <linubran90@gmail.com>
2023-05-23 10:16:04 +0200

    ads new env (DOCUMENT_BASE_URL)

--- components/blocks/Media.tsx
+++ components/blocks/Media.tsx
@@ -11,12 +11,17 @@ import { MediaBase } from "../../graphql/__generated__/MediaBase";
 import { isExternalLink } from "../../utilities/checkers";
 import { CustomImage } from "../Image";
 
-export const handleUrl = ({ screen9, url }: MediaBase) => {
+export const handleUrl = ({ screen9, url, __typename }: MediaBase) => {
+  const documentBaseUrl = env("DOCUMENT_BASE_URL");
+  const mediaBaseUrl = env("MEDIA_BASE_URL");
+
   if (screen9 || isExternalLink(url)) {
     return url;
+  } else if (__typename === "dataportal_Digg_File") {
+    return (documentBaseUrl || mediaBaseUrl || "") + `${url}`;
   }
 
-  return (env("MEDIA_BASE_URL") || "") + `${url}`;
+  return (mediaBaseUrl || "") + `${url}`;
 };
 
 const renderMedia = (media: Media_media, mediaDescription?: string) => {
@@ -29,7 +34,11 @@ const renderMedia = (media: Media_media, mediaDescription?: string) => {
       }
       return (
         <figure>
-          <CustomImage image={media} style="responsive" sizes={{mobile: "100vw", tablet: "100vw", desktop: "50vw"}} />
+          <CustomImage
+            image={media}
+            style="responsive"
+            sizes={{ mobile: "100vw", tablet: "100vw", desktop: "50vw" }}
+          />
           {mediaDescription && (
             <figcaption>
               <p className="media-description">{checkLang(mediaDescription)}</p>
