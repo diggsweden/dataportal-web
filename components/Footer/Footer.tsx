98f2e00193f68dba2f1e1ccbb1a9fcdd38942899
linulas <linubran90@gmail.com>
2023-06-15 12:18:19 +0200

    ads eu logo in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,6 +3,8 @@ import useTranslation from 'next-translate/useTranslation';
 import React from 'react';
 import { Footer_dataportal_v1_Digg_Footer_columns } from '../../graphql/__generated__/Footer';
 import { renderMarkdown } from '../Renderers';
+import Image from "next/image";
+import euLogo from "../../public/images/eu.png";
 
 export interface FooterProps {
   columns: (Footer_dataportal_v1_Digg_Footer_columns | null)[];
@@ -40,17 +42,21 @@ export const Footer: React.FC<FooterProps> = ({ columns }) => {
           </div>
 
           <div className="digg__">
-            <div
-              css={css`
-                width: 15rem;
-                ${space({ mr: 4 })};
-              `}
-            >
-              <DiggLogo
-                title={t('digg-logo_text')}
-                id="footer"
-                mode="wide"
-                width={30 * 16}
+            <div className="footer__logos">
+              <div className="footer__logos-digg">
+                <DiggLogo
+                  title={t('common|digg-logo_text')}
+                  id="footer"
+                  mode="wide"
+                  width={30 * 16}
+                />
+              </div>
+              <Image
+                src={euLogo}
+                alt=""
+                width={200}
+                height={50}
+                priority={true}
               />
             </div>
           </div>
