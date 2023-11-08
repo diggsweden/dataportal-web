9b40aaa118b9edeba5098846db4fa5f634f6d380
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2023-11-08 10:32:34 +0100

    MID-52 Change sandbox urls in dataportalen.se

--- env/Settings.Sandbox.ts
+++ env/Settings.Sandbox.ts
@@ -3,13 +3,13 @@ import { EnvSettings } from "./EnvSettings";
 
 export class Settings_Sandbox extends EnvSettings {
   ENTRYSCAPE_DATASETS_PATH="sandbox.admin.dataportal.se"
-  ENTRYSCAPE_SPECS_PATH="editera.dataportal.se"
+  ENTRYSCAPE_SPECS_PATH="sandbox.editera.dataportal.se"
   ENTRYSCAPE_TERMS_PATH="sandbox.editera.dataportal.se"
   ENTRYSCAPE_ORG_STATS_URL="https://sandbox.admin.dataportal.se/charts/orgData.json"
   ENTRYSCAPE_THEME_STATS_URL="https://sandbox.admin.dataportal.se/charts/themeData.json"
   ENTRYSCAPE_THEME_STATS_URL_EN="https://sandbox.admin.dataportal.se/charts/themeData_en.json"
 
-  ENTRYSCAPE_CONCEPT_STATS_URL="https://editera.dataportal.se/stats/entityData.json"
+  ENTRYSCAPE_CONCEPT_STATS_URL="https://sandbox.editera.dataportal.se/stats/entityData.json"
   ENTRYSCAPE_HISTORY_STATS_URL="https://sandbox.admin.dataportal.se/stats/historyData.json"
 
   ENTRYSCAPE_BLOCKS_URL="https://static.cdn.entryscape.com/blocks/0.20/app.js"
