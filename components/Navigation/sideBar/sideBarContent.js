13673e6be61083c62f98c99bb6d5a8993f0e49f0
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2023-11-24 15:55:42 +0100

    DIGG-112 sidebar fix

--- components/Navigation/sideBar/sideBarContent.js
+++ components/Navigation/sideBar/sideBarContent.js
@@ -1,11 +1,13 @@
 import home from "./menu-icons/home.svg";
 import data from "./menu-icons/data.svg";
-import utbildning from "./menu-icons/utbildning.svg";
+// import utbildning from "./menu-icons/utbildning.svg";
 import delaData from "./menu-icons/dela-data.svg";
 import stodVerktyg from "./menu-icons/stod-verktyg.svg";
-import verktyg from "./menu-icons/verktyg.svg";
+// import verktyg from "./menu-icons/verktyg.svg";
 import nyheter from "./menu-icons/nyheter.svg";
 import community from "./menu-icons/community.svg";
+import datasamverkan from "./menu-icons/datasamverkan.svg";
+import godaExempel from "./menu-icons/godaExempel.svg";
 
 export const sideBarConstants = [
   {
@@ -47,69 +49,82 @@ export const sideBarConstants = [
       },
     ],
   },
-  // {
-  //   title: "Goda exempel",
-  //   icon: godaExempel,
-  //   href: "goda-exempel",
-  //   external: false,
-  // },
-  // {
-  //   title: "Datasamverkan",
-  //   icon: datasamverkan,
-  //   href: "datasamverkan",
-  //   external: false,
-  // },
+  {
+    title: "good-examples",
+    icon: godaExempel,
+    id: 7,
+    children: [
+      {
+        title: "good-examples",
+        href: "/goda-exempel",
+        external: false,
+        id: 8,
+      },
+      {
+        title: "contribute",
+        href: "/var-med-och-bidra",
+        external: false,
+        id: 9,
+      },
+    ],
+  },
+  {
+    title: "data-collaboration",
+    icon: datasamverkan,
+    href: "datasamverkan",
+    external: false,
+  },
   {
     title: "why-share-data",
     icon: delaData,
-    id: 7,
+    id: 10,
     children: [
       {
         title: "shara-data-benefits",
         href: "/nyttor-med-datadelning",
         external: false,
-        id: 8,
+        id: 11,
       },
       {
         title: "legislation",
         href: "/lagstiftning",
         external: false,
-        id: 9,
+        id: 12,
       },
       {
         title: "governance",
         href: "/styrning",
         external: false,
-        id: 10,
+        id: 13,
       },
     ],
   },
-  {
-    title: "self-assessment-tools",
-    icon: verktyg,
-    href: "/verktyg-for-sjalvutvardering",
-    external: false,
-    id: 11,
-  },
-  {
-    title: "training",
-    icon: utbildning,
-    href: "/utbildning-for-chefer-och-ledare",
-    external: false,
-    id: 12,
-  },
+  // {
+  //   title: "self-assessment-tools",
+  //   icon: verktyg,
+  //   href: "/verktyg-for-sjalvutvardering",
+  //   external: false,
+  //   id: 14,
+  // },
+  // {
+  //   title: "training",
+  //   icon: utbildning,
+  //   href: "/utbildning-for-chefer-och-ledare",
+  //   external: false,
+  //   id: 15,
+  // },
   {
     title: "news",
     icon: nyheter,
     href: "/nyheter",
     external: false,
-    id: 13,
+    id: 14,
   },
   {
     title: "community",
     icon: community,
     href: "https://community.dataportal.se/",
     external: true,
-    id: 14,
+    id: 15,
   },
 ];
