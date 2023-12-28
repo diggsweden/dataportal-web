7edfab34f18eee094b31575b4f9467ca1f438380
Konstantin Pinaev <konstantin.pinaev@gmail.com>
2023-12-28 19:30:09 +0100

    DIGG-58: Building search selector

--- components/content/Search/SearchPageSelector/index.tsx
+++ components/content/Search/SearchPageSelector/index.tsx
@@ -1,7 +1,7 @@
 import useTranslation from "next-translate/useTranslation";
 import { useRouter } from "next/router";
 import React from "react";
-import { ButtonLink } from "@/components/global/Button";
+import { Tab } from "@/components/global/Tab";
 
 interface SearchTabsProps {
   activeLink?: string;
@@ -14,28 +14,30 @@ export const SearchPageSelector: React.FC<SearchTabsProps> = ({ query }) => {
   const { pathname } = useRouter() || {};
   return (
     <div className="flex space-x-md">
-      <ButtonLink
-        variant="secondary"
+      <Tab
         href={`${lang}/datasets?q=${query ? query : ""}&f=`}
-        className={`text-md ${pathname == "/datasets" ? "active" : ""} `}
-        label={t("search$datasets")}
-      />
-      <ButtonLink
+        active={pathname === "/datasets"}
+      >
+        {t("search$datasets")}
+      </Tab>
+      <Tab
         href={`${lang}/concepts?q=${query ? query : ""}&f=`}
-        className={`text-md ${pathname == "/concepts" ? "active" : ""} `}
-        label={t("search$concepts")}
-      />
-      <ButtonLink
+        active={pathname === "/concepts"}
+      >
+        {t("search$concepts")}
+      </Tab>
+      <Tab
         href={`${lang}/specifications?q=${query ? query : ""}&f=`}
-        className={`text-md ${pathname == "/specifications" ? "active" : ""} `}
-        label={t("search$specifications")}
-      />
-      <ButtonLink
-        variant="secondary"
+        active={pathname === "/specifications"}
+      >
+        {t("search$specifications")}
+      </Tab>
+      <Tab
         href={`${lang}/search?q=${query ? query : ""}&f=`}
-        className={`text-md ${pathname == "/datasets" ? "active" : ""} `}
-        label={"Sök efter innehåll"}
-      />
+        active={pathname === "/search"}
+      >
+        {"Sök efter innehåll"}
+      </Tab>
     </div>
   );
 };
