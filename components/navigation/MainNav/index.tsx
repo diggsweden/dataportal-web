b37f74c6446bc5d2fdbf07aad9ea2ac858508d9b
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2024-01-16 15:41:15 +0100

    DIGG-209: focus-visible

--- components/navigation/MainNav/index.tsx
+++ components/navigation/MainNav/index.tsx
@@ -80,7 +80,7 @@ const MainNav: FC<MainNavProps> = ({ setOpenSideBar, openSideBar }) => {
           {!openSearch ? (
             <Button
               onClick={() => setOpenSearch(!openSearch)}
-              className="cursor-pointer p-[10px] hover:bg-brown-800"
+              className="cursor-pointer p-[10px] hover:bg-brown-800 focus-visible:-outline-offset-2 focus-visible:outline-white"
             >
               <SearchIcon />
             </Button>
