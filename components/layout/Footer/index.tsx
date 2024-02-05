6726fe2fe17b443132098605076061a40aff11a8
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2024-02-05 11:30:12 +0100

    DIGG-252: Remove Digg logo and add new footer text with link

--- components/layout/Footer/index.tsx
+++ components/layout/Footer/index.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
 import Image from "next/image";
-import DiggLogo from "@/assets/logos/diggLarge.svg";
 import { FooterNav } from "@/components/navigation/FooterNav";
 import { Container } from "@/components/layout/Container";
+import Link from "next/link";
+import useTranslation from "next-translate/useTranslation";
 
 type FooterProps = {
   openSideBar: boolean;
@@ -10,6 +11,7 @@ type FooterProps = {
 };
 
 export const Footer: FC<FooterProps> = ({ openSideBar, setOpenSideBar }) => {
+  const { t } = useTranslation();
   return (
     <footer className="mt-xl border-t-[1px] border-brown-200 bg-white py-xl">
       <div
@@ -20,7 +22,16 @@ export const Footer: FC<FooterProps> = ({ openSideBar, setOpenSideBar }) => {
         <Container className="space-y-xl md:space-y-2xl">
           <FooterNav setOpenSideBar={setOpenSideBar} />
           <div className="flex flex-col justify-between gap-xl align-top md:flex-row md:gap-none">
-            <DiggLogo aria-label="Digg logo" />
+            <span>
+              {t("common|managed-and-developed-by")}
+              <Link
+                aria-label="Digg logo"
+                href={"https://digg.se/"}
+                className="focus--red text-green-600"
+              >
+                Digg – Myndigheten för digital förvaltning
+              </Link>
+            </span>
             <Image
               src={"/images/europeiskaunionen.png"}
               width={200}
