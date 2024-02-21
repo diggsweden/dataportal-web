817664b61dc4f09a79dd068fc578ad5acd3b6a98
Mika Munterud <mika.munterud@digitalist.se>
2024-02-21 11:10:47 +0100

    DIGG-262-beta: Updating yabindex on dataset buttons

--- components/content/Entryscape/DataSetPage/index.tsx
+++ components/content/Entryscape/DataSetPage/index.tsx
@@ -217,7 +217,7 @@ export const DataSetPage: React.FC = () => {
                     "pages|datasetpage$several_links",
                   )}{{/ifprop}}' +
                   '{{#ifprop "dcat:downloadURL" min="2" invert="true"}}' +
-                  '<a href="{{prop "dcat:downloadURL"}}" class="text-white noUnderline">' +
+                  '<a href="{{prop "dcat:downloadURL"}}" tabindex="-1" class="text-white noUnderline">' +
                   '<button class="button--primary button--large text-white flex items-center !no-underline">' +
                   '${t("pages|datasetpage$download_link")}' +
                   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">' +
@@ -228,7 +228,7 @@ export const DataSetPage: React.FC = () => {
                   '{{/ifprop}}' +
                   '{{/ifprop}}' +
                   '{{#ifprop "dcat:downloadURL" invert="true"}}' +
-                  '<a href="{{prop "dcat:accessURL"}}" class="text-white noUnderline">' +
+                  '<a href="{{prop "dcat:accessURL"}}" tabindex="-1" class="text-white noUnderline">' +
                   '<button class="button--primary button--large text-white flex items-center !no-underline">' +
                   '${t("pages|datasetpage$download_link_adress")}' +
                   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">' +
@@ -267,7 +267,7 @@ export const DataSetPage: React.FC = () => {
                 relation: 'dcat:accessService',
                 template: 
                 '<button class="button--primary button--large flex items-center !no-underline">' +
-                  '{{link class="linkInBtn noUnderline" tabindex="-1" namedclick="dataservice-link" content="${t(
+                  '{{link class="linkInBtn noUnderline" namedclick="dataservice-link" content="${t(
                     "pages|datasetpage$read_about_api",
                   )}"}}' +
                   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">' +
@@ -518,13 +518,13 @@ export const DataSetPage: React.FC = () => {
                 data-entryscape-block="template"
                 dangerouslySetInnerHTML={{
                   __html: `
-                      <a class="text-white noUnderline" href="{{metadataURI}}?recursive=dcat">
-                      <span class="button--primary button--large text-white flex items-center !no-underline">
+                      <a class="text-white noUnderline" tabindex="-1" href="{{metadataURI}}?recursive=dcat">
+                      <button class="button--primary button--large text-white flex items-center !no-underline">
                       ${t("pages|datasetpage$rdf")}
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                       <path d="M12 16L7 11L8.4 9.55L11 12.15V4H13V12.15L15.6 9.55L17 11L12 16ZM6 20C5.45 20 4.97917 19.8042 4.5875 19.4125C4.19583 19.0208 4 18.55 4 18V15H6V18H18V15H20V18C20 18.55 19.8042 19.0208 19.4125 19.4125C19.0208 19.8042 18.55 20 18 20H6Z" fill="#F0EFEE"/>
                       </svg>
-                      </span>
+                      </button>
                       </a>
                       `,
                 }}
