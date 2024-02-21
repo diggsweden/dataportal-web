c3afc417d92bfda2eb959cb8a12efbdeaea07fab
Mika Munterud <mika.munterud@digitalist.se>
2024-02-21 11:22:13 +0100

    DIGG-262-beta: Updating tabindex on specifications buttons

--- components/content/Entryscape/SpecificationPage/index.tsx
+++ components/content/Entryscape/SpecificationPage/index.tsx
@@ -196,6 +196,23 @@ export const SpecificationPage: FC<{
                 }/theme/templates/prof.json',
               ],
             },
+            collections: [
+              {
+                type: 'facet',
+                name: 'theme',
+                label: 'Theme',
+                property: 'dcat:theme',
+                nodetype: 'uri',
+                templatesource: 'dcat:theme-isa',
+              },
+              {
+                type: 'facet',
+                name: 'keyword',
+                label: 'Keyword',
+                property: 'dcat:keyword',
+                nodetype: 'literal',
+              }
+            ],
             blocks: [
               ${hemvist(t)},
               ${keyword(t)},
@@ -212,12 +229,12 @@ export const SpecificationPage: FC<{
                 '<span>{{text}}</span>' + 
                   '<span class="block mb-md">{{prop "prof:hasRole" class="type" render="label"}}</span>' +
                   '<div>{{ text content="\${skos:definition}" }}</div>' +
-                  '<a href="{{resourceURI}}"><span class="button button--primary button--large text-white">${t(
+                  '<a href="{{resourceURI}}" tabindex="-1"><button class="button button--primary button--large text-white">${t(
                     "pages|specification_page$specification_download",
                   )}' +
                   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">' +
                   '<path d="M12 16L7 11L8.4 9.55L11 12.15V4H13V12.15L15.6 9.55L17 11L12 16ZM6 20C5.45 20 4.97917 19.8042 4.5875 19.4125C4.19583 19.0208 4 18.55 4 18V15H6V18H18V15H20V18C20 18.55 19.8042 19.0208 19.4125 19.4125C19.0208 19.8042 18.55 20 18 20H6Z" fill="#FFFFFF"/>' +
-                  '</svg></span></a>',
+                  '</svg></button></a>',
               },
             ],
           }];
