802238c99b14c944707653ba75b5064187178811
Mika Munterud <mika.munterud@digitalist.se>
2024-02-23 08:31:33 +0100

    DIGG-279-beta: Updating styling for first header on text-block

--- components/global/Typography/HtmlParser/index.tsx
+++ components/global/Typography/HtmlParser/index.tsx
@@ -89,7 +89,9 @@ export const HtmlParser: FC<{ text: string }> = ({ text }) => {
               ? "!mt-lg md:!mt-xl !mb-md md:!mb-lg"
               : name === "h2"
               ? "!mb-md md:!mb-lg"
-              : "!mt-md md:!mt-lg !mb-md";
+              : name === "h3" && prev
+              ? "!mt-md md:!mt-lg !mb-md"
+              : "!mb-md";
           return (
             // @ts-ignore
             <Heading level={level} size={size} className={className}>
