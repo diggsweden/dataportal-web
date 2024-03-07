7d6506b1ba618b85b30b392ef71983e71fe2e3bf
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2024-03-07 09:26:18 +0100

    DIGG-291: Changing back to pink as Infoblock

--- components/global/Typography/HtmlParser/index.tsx
+++ components/global/Typography/HtmlParser/index.tsx
@@ -65,7 +65,7 @@ export const HtmlParser: FC<{ text: string }> = ({ text }) => {
         if (attribs.class === "infoblock") {
           return (
             <div
-              className={`!my-lg border border-l-[6px] border-green-600 bg-white p-lg ${attribs.class}`}
+              className={`!my-lg border border-l-[6px] border-primary p-lg ${attribs.class}`}
             >
               {domToReact(children as DOMNode[], options)}
             </div>
@@ -75,7 +75,7 @@ export const HtmlParser: FC<{ text: string }> = ({ text }) => {
         if (attribs.class === "border") {
           return (
             <div
-              className={`!my-lg border border-l-[6px] border-primary p-lg ${attribs.class}`}
+              className={`!my-lg border border-l-[6px] border-green-600 bg-white p-lg ${attribs.class}`}
             >
               {domToReact(children as DOMNode[], options)}
             </div>
