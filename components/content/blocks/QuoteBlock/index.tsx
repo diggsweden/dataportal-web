8b3cfe6675a633ed160aa6478b81667cee0a98be
Mika Munterud <mika.munterud@digitalist.se>
2024-03-11 16:14:41 +0100

    DIGG-161-bet: Updating styling on Quote block.

--- components/content/blocks/QuoteBlock/index.tsx
+++ components/content/blocks/QuoteBlock/index.tsx
@@ -5,10 +5,10 @@ import { CustomImage } from "@/components/global/CustomImage";
 
 export const QuoteBlock: FC<QuoteFragment> = ({ quote, author, image }) => {
   return (
-    <blockquote className="flex gap-md lg:gap-lg">
-      <div className="grid auto-cols-auto text-md font-normal text-pink-600 md:gap-lg md:text-lg lg:text-xl">
-        <QuoteIcon className="flex w-[40px] scale-50 md:scale-75 lg:scale-100" />
-        <div className="col-start-2 mt-sm flex w-full flex-col space-y-md divide-y md:mt-xs">
+    <blockquote className="flex flex-row items-center gap-lg lg:gap-xl">
+      <div className="flex h-fit w-full text-md font-normal text-pink-600 md:gap-lg md:text-lg lg:text-xl">
+        <QuoteIcon className="flex w-full max-w-[40px] scale-50 md:scale-75 lg:scale-100" />
+        <div className="mt-sm flex w-full flex-col space-y-md divide-y md:mt-xs">
           <span>{quote}</span>
           <span className="border-brown-200 pt-md text-md text-brown-600 md:text-lg">
             {author}
