161e88b77b7e22d862198928fb839f0cb08d707d
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2024-03-11 13:15:57 +0100

    DIGG-161: Quote block

--- components/content/blocks/QuoteBlock/index.tsx
+++ components/content/blocks/QuoteBlock/index.tsx
@@ -1,27 +1,26 @@
 import { FC } from "react";
 import QuoteIcon from "@/assets/icons/quote.svg";
-import { DOMNode } from "html-react-parser";
+import { QuoteFragment } from "@/graphql/__generated__/operations";
+import { CustomImage } from "@/components/global/CustomImage";
 
-const QuoteBlock: FC<{ children: DOMNode[] }> = ({ children }) => {
+export const QuoteBlock: FC<QuoteFragment> = ({ quote, author, image }) => {
   return (
-    <blockquote className="grid auto-cols-auto gap-lg text-xl font-normal text-pink-600">
-      <QuoteIcon className="flex w-[40px]" />
-      <div className="col-start-2 flex w-full flex-col space-y-md divide-y">
-        {children && children.length > 0 ? (
-          children.map((child: any, idx: number) => (
-            <span
-              key={idx}
-              className="last:pt-md last:text-md last:text-brown-600"
-            >
-              {child}
-            </span>
-          ))
-        ) : (
-          <>{children}</>
-        )}
+    <blockquote className="flex gap-md lg:gap-lg">
+      <div className="grid auto-cols-auto text-md font-normal text-pink-600 md:gap-lg md:text-lg lg:text-xl">
+        <QuoteIcon className="flex w-[40px] scale-50 md:scale-75 lg:scale-100" />
+        <div className="col-start-2 mt-sm flex w-full flex-col space-y-md divide-y md:mt-xs">
+          <span>{quote}</span>
+          <span className="border-brown-200 pt-md text-md text-brown-600 md:text-lg">
+            {author}
+          </span>
+        </div>
       </div>
+      {image && (
+        <CustomImage
+          image={image || {}}
+          className="h-[50px] max-w-[50px] rounded-full object-cover md:h-[100px] md:max-w-[100px] lg:h-[150px] lg:max-w-[150px]"
+        />
+      )}
     </blockquote>
   );
 };
-
-export default QuoteBlock;
