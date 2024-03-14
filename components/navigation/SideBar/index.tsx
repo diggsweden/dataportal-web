287028ee58429183a5b46ac08efeda1e7073ed3c
Mika Munterud <mika.munterud@digitalist.se>
2024-03-14 13:48:48 +0100

    DIGG-300-beta: Updating sidebar visibility when hidden.

--- components/navigation/SideBar/index.tsx
+++ components/navigation/SideBar/index.tsx
@@ -63,11 +63,9 @@ export const SideBar: FC<NavSideProps> = ({ openSideBar, setOpenSideBar }) => {
     if (sidebar) {
       if (openSideBar) {
         sidebar.classList.remove("hidden");
-        setTimeout(() => {
-          sidebar.classList.add("translate-x-none");
-        }, 1);
+        sidebar.classList.remove("translate-x-full");
       } else {
-        sidebar.classList.remove("translate-x-none");
+        sidebar.classList.add("translate-x-full");
         setTimeout(() => {
           sidebar.classList.add("hidden");
         }, 300);
@@ -79,8 +77,10 @@ export const SideBar: FC<NavSideProps> = ({ openSideBar, setOpenSideBar }) => {
     <nav
       id="sidebar"
       ref={ref}
-      className={`absolute right-none top-[136px] z-50 -mb-[136px] h-[calc(100%-136px)] w-full translate-x-full 
-       overflow-y-scroll bg-white transition-transform duration-300 ease-in-out md:w-[300px]`}
+      className={`absolute right-none top-[136px] z-50 -mb-[136px] h-[calc(100%-136px)] w-full
+      overflow-y-scroll bg-white transition-transform duration-300 ease-in-out md:w-[300px] ${
+        openSideBar ? "" : ""
+      }`}
       aria-label="Sidebar"
     >
       <ul className="w-full list-none">
