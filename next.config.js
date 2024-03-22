de4f867b5546abde8c5d94511229aaaee9b2d0ad
Mika Munterud <mika.munterud@digitalist.se>
2024-03-22 19:16:20 +0100

    Adding image domain to next config.

--- next.config.js
+++ next.config.js
@@ -86,7 +86,11 @@ const nextConfig = nextTranslate({
   staticPageGenerationTimeout: 240,
 
   images: {
-    domains: [process.env.IMAGE_DOMAIN || "localhost", "bcdn.screen9.com"],
+    domains: [
+      process.env.IMAGE_DOMAIN || "localhost",
+      "bcdn.screen9.com",
+      "dev.beta.strapi.dataportal.dev1.se",
+    ],
     deviceSizes: [640, 828, 1080, 1200, 1920, 3840],
     imageSizes: [128, 256, 384],
     dangerouslyAllowSVG: true,
