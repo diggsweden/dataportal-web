c8ca28df42cd3f5a626ad3d47d9f7863aeeae88c
Mika Munterud <mika.munterud@digitalist.se>
2024-03-25 08:57:08 +0100

    Reverting next config to previous image domain settings

--- next.config.js
+++ next.config.js
@@ -86,12 +86,8 @@ const nextConfig = nextTranslate({
   staticPageGenerationTimeout: 240,
 
   images: {
-    domains: [
-      process.env.IMAGE_DOMAIN || "localhost",
-      "bcdn.screen9.com",
-      "dev.beta.strapi.dataportal.dev1.se",
-    ],
-    deviceSizes: [640, 828, 1080, 1200, 1920, 3840],
+    domains: [process.env.IMAGE_DOMAIN || "localhost", "bcdn.screen9.com"],
+    deviceSizes: [640, 1080, 1200, 1920, 3840],
     imageSizes: [128, 256, 384],
     dangerouslyAllowSVG: true,
     path: "/_next/image",
