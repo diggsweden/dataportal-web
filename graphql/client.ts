71709a4b18fc088166ba1a33c6ec0e1ff6b5b464
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2024-07-02 09:07:17 +0200

    apollo link logger

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,8 +1,18 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import reactEnv from "@beam-australia/react-env";
+import { ApolloLink } from "@apollo/client/core";
+import { createHttpLink } from "@apollo/client/link/http";
+import loggerLink from "./loggerLink";
+
+const httpLink = createHttpLink({
+  uri: reactEnv("APOLLO_URL") || "",
+});
+
+const link = ApolloLink.from([loggerLink, httpLink]);
 
 export const browserclient = new ApolloClient({
   uri: reactEnv("APOLLO_URL") || "",
+  link,
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
@@ -13,6 +23,7 @@ export const browserclient = new ApolloClient({
 
 export const client = new ApolloClient({
   uri: process.env.APOLLO_URL || "",
+  link,
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
--- graphql/loggerLink.ts
+++ graphql/loggerLink.ts
@@ -1,30 +1,35 @@
+/* eslint-disable no-console */
 import { ApolloLink, Operation, NextLink, FetchResult } from "@apollo/client";
-import { Observable } from "apollo-link";
+import { Observable } from "@apollo/client/utilities";
 
-const loggerLink = new ApolloLink((operation: any, forward: any) => {
-  console.log(`Starting request for ${operation.operationName}`);
-  console.log('Query:', operation.query);
-  console.log('Variables:', operation.variables);
+const loggerLink = new ApolloLink(
+  (operation: Operation, forward: NextLink): Observable<FetchResult> => {
+    console.log(`Starting request for ${operation.operationName}`);
+    console.log("Query:", operation.query);
+    console.log("Variables:", operation.variables);
 
-  return new Observable<FetchResult>((observer) => {
-    const subscription = forward(operation).subscribe({
-      next: (result: any): void => {
-        console.log(`Received response for ${operation.operationName}`);
-        console.log("Result:", result);
-        observer.next(result);
-      },
-      error: (error: any) => {
-        console.error(`Error in request for ${operation.operationName}`);
-        console.error("Error:", error);
-        observer.error(error);
-      },
-      complete: observer.complete.bind(observer),
+    return new Observable<FetchResult>((observer) => {
+      const subscription = forward(operation).subscribe({
+        next: (result: FetchResult) => {
+          console.log(`Received response for ${operation.operationName}`);
+          console.log("Result:", result);
+          observer.next(result);
+        },
+        error: (error: Error) => {
+          console.error(`Error in request for ${operation.operationName}`);
+          console.error("Error:", error);
+          observer.error(error);
+        },
+        complete: () => {
+          observer.complete();
+        },
+      });
+
+      return () => {
+        subscription.unsubscribe();
+      };
     });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  });
-});
+  },
+);
 
 export default loggerLink;
