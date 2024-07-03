a163a68f15496966d6259c648b1df9326361f10b
Michael Axelsson <michael.axelsson@digitalistgroup.com>
2024-07-03 12:24:45 +0200

    DIGG-421: Add tests for dataset search.

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -18,3 +18,20 @@ import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// Hide fetch/XHR requests from command log
+if (Cypress.config("hideXHRInCommandLog")) {
+  const app = window.top;
+
+  if (
+    app &&
+    !app.document.head.querySelector("[data-hide-command-log-request]")
+  ) {
+    const style = app.document.createElement("style");
+    style.innerHTML =
+      ".command-name-request, .command-name-xhr { display: none }";
+    style.setAttribute("data-hide-command-log-request", "");
+
+    app.document.head.appendChild(style);
+  }
+}
