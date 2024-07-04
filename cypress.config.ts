05c67c180db8cee618e25c6997c8f250518e1c91
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2024-07-04 09:34:02 +0200

    DIGG-424 concept page test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "cypress";
+import { verifyDownloadTasks } from "cy-verify-downloads";
+import { rm } from "fs";
 
 export default defineConfig({
   e2e: {
     // This will automatically prefix cy.visit() and cy.request() commands with this baseUrl.
     baseUrl: "http://localhost:3000",
     hideXHRInCommandLog: true,
+    setupNodeEvents(on) {
+      on("task", verifyDownloadTasks);
+      on("task", {
+        deleteFolder(folderName) {
+          return new Promise((resolve) => {
+            rm(folderName, { maxRetries: 10, recursive: true }, (err) => {
+              if (err) {
+                console.error(err);
+              }
+              resolve(null);
+            });
+          });
+        },
+      });
+    },
   },
 });
