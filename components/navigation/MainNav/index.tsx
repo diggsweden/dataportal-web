a8a1f506ccc6b8d956a3bd0d0ef3d871d638eae6
Mika Munterud <mika.munterud@outlook.com>
2024-11-07 13:15:10 +0100

    lint-fix

--- components/navigation/MainNav/index.tsx
+++ components/navigation/MainNav/index.tsx
@@ -74,7 +74,7 @@ const MainNav: FC<MainNavProps> = ({ setOpenSideBar, openSideBar }) => {
             className={`${
               openSearch
                 ? "hidden lg:block"
-                : "max-h-[2rem] w-full min-w-0 max-w-[10rem] md:h-[2.75rem] md:w-[15.5rem]"
+                : "min-w-0 max-h-[2rem] w-full max-w-[10rem] md:h-[2.75rem] md:w-[15.5rem]"
             }`}
           />
         )}
