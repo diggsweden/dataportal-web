9c27fb45b06a579f6e4648a4692953db06f8a67d
Alessandro Gasperini <alessandro@digitalists-MacBook-Pro-2.local>
2024-12-05 15:39:25 +0100

    DIGG-472: Follow specification facet

--- components/typography/body-variant/index.tsx
+++ components/typography/body-variant/index.tsx
@@ -64,6 +64,7 @@ const BodyVariant: FC<PropsWithChildren<BodyProps>> = ({
   const CustomTag = variant as ElementType;
 
   return (
+    // @ts-expect-error
     <CustomTag className={cx(bodyVariants({ variant }), className)}>
       {variant === "blockquote" && <QuoteIcon className="w-xl flex-shrink-0" />}
       {children}
